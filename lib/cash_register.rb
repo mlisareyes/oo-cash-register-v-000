5a2cbe431f94154af2447d351255ffd835749712
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-10 05:06:21 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,8 +13,20 @@ class CashRegister
   end
 
   def apply_discount
+    if self.discount != 0
+      self.total = (self.total * (100-self.discount))/100
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
+    end
   end
 
-
-
+  def void_last_transaction
+    if self.last_transaction != {}
+      self.items.pop(self.last_transaction[:quantity])
+      self.total -= self.last_transaction[:price]*self.last_transaction[:quantity]
+    else
+      "You haven't entered any transactions yet."
+    end
+  end
 end
