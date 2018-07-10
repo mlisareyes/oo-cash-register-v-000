f551e69dc7b3c0b1f3a61fa03ae71abba3225bdb
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-10 03:46:32 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
 
+class CashRegister
+  attr_accessor :total, :discount, :items, :last_transaction
+
+  def initialize(discount = 0)
+    self.total = 0
+    self.discount = discount
+    
+  end
+
+end
